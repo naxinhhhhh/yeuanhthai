8caa032270927a3c4b63a553fc707567a39d9a93
Na mê Cu <127460762+NaMeCuOwO@users.noreply.github.com>
2023-04-10 20:20:39 +0700

    Update yeuanhthai.py

--- yeuanhthai.py
+++ yeuanhthai.py
@@ -1,2 +1,9 @@
-print("Area =", 6.4 * 7.8)
-print("Perimeter =", (6.4 + 7.8) * 2)
+n = int(input())
+lst = []
+for i in range(n):
+    lst.append(int(input()))
+min_value = lst[0]
+for i in lst:
+    if i < min_value:
+        min_value = i
+print(min_value)
