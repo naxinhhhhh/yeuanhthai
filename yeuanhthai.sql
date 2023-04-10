597817b28fa2c300e91f56aa77b27d61bee20d7b
Na mê Cu <127460762+NaMeCuOwO@users.noreply.github.com>
2023-04-10 20:07:34 +0700

    Delete yeuanhthai.sql

--- yeuanhthai.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employees(
-    emp_id SERIAL PRIMARY KEY,
-    emp_name VARCHAR(10),
-    hire_date DATE DEFAULT '2023-04-10'
-    salary DECIMAL(7,2),
-    com DECIMAL(7,2) DEFAULT 0.03
-);
